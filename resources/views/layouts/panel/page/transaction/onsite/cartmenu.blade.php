2020b2eb865a248a854b7fb9e3a6bdd78a05ea90
rairizky <32klolita@gmail.com>
2020-10-02 16:36:10 +0700

    add topping layout

--- resources/views/layouts/panel/page/transaction/onsite/cartmenu.blade.php
+++ resources/views/layouts/panel/page/transaction/onsite/cartmenu.blade.php
@@ -103,8 +103,6 @@
                                         <div class="dropdown">
                                             <a class="toggle" id="dropdownActionOrder" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#"><i class="fa fa-caret-down"></i><span class="badge"></span></a>
                                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownActionOrder">
-                                                <a class="dropdown-item" href="#">Add Topping</a>
-                                                <hr>
                                                 <a class="dropdown-item" href="#">Edit</a>
                                                 <hr>
                                                 <a class="dropdown-item" href="{{ route('panel.transaction.deletemenuorder.onsite', [$current_customer->id, $list->id]) }}">Delete</a>
@@ -128,7 +126,24 @@
                         </p>
                     </div>
                 </div>
-                <div class="mt-4 box">
+                <div class="mt-3 box">
+                    <div class="box-head">
+                        <div class="d-flex justify-content-between row mbn-20 mr-1 ml-1 pb-3">
+                            <h4 class="title">Extra Topping</h4>
+                            <a href="#">
+                                <span class="badge badge-pill badge-primary"><i class="zmdi zmdi-plus"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="box-body">
+
+                        <hr>
+                        <p>
+                            <strong>Total 0 extra toppings.</strong>
+                        </p>
+                    </div>
+                </div>
+                <div class="mt-3 box">
                     <div class="box-head">
                         <h4 class="title">Payment</h4>
                     </div>
